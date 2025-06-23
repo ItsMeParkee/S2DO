92dda265a93709c912e3adcded6ead7fae2f6abf
ItsMeParkee <savageparker05@gmail.com>
2025-06-23 09:46:03 -0500

    Fix Google auth initialization

--- script.js
+++ script.js
@@ -5,12 +5,54 @@ document.getElementById('themeToggle').onclick = () => {
 
 let chartInstance;
 
-document.getElementById('loginBtn').onclick = () => {
-  document.getElementById('userEmail').innerText = "you@example.com";
-  document.getElementById('subscriberCount').innerText = "1,204";
-  document.getElementById('viewCount').innerText = "589,321";
-  drawChart();
-};
+const CLIENT_ID = 'YOUR_CLIENT_ID'; // TODO: replace with your OAuth client id
+const API_KEY = 'YOUR_API_KEY'; // TODO: replace with your API key
+
+const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'];
+const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+  gapi.client
+    .init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES,
+    })
+    .then(() => {
+      gapi.client.load('youtube', 'v3').then(() => {
+        const authInstance = gapi.auth2.getAuthInstance();
+        updateSigninStatus(authInstance.isSignedIn.get());
+        document.getElementById('loginBtn').onclick = () =>
+          authInstance.signIn().then(loadChannelStats);
+      });
+    });
+}
+
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    loadChannelStats();
+  }
+}
+
+function loadChannelStats() {
+  gapi.client.youtube.channels
+    .list({
+      part: 'snippet,statistics',
+      mine: true,
+    })
+    .then((response) => {
+      const channel = response.result.items[0];
+      document.getElementById('userEmail').innerText = channel.snippet.title;
+      document.getElementById('subscriberCount').innerText = channel.statistics.subscriberCount;
+      document.getElementById('viewCount').innerText = channel.statistics.viewCount;
+      drawChart();
+    });
+}
 
 function drawChart() {
   const ctx = document.getElementById('videoChart').getContext('2d');
